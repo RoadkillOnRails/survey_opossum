095d88de637778078a8aabd579f254c188df3802
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-10 17:04:48 -0500

    author has a name function

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,12 @@
 class Author < ActiveRecord::Base
   has_secure_password
-  
+
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  has_many :surveys
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
