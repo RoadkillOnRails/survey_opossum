f0932887ccfc60ae0254323bfb20a6f5dcf1301f
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-10 18:16:07 -0500

    Added validation to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,13 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   belongs_to :question_type
+  has_many :options, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
-  has_many :options
-  has_many :answers
+  validates :question_text, presence: true
+  validates :required, presence: true
+  validates :order, presence: true
+  validates :question_type_id, presence: true
 
   accepts_nested_attributes_for :options,
       reject_if: :all_blank,
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
   belongs_to :author
-  has_many :questions
+  has_many :questions, dependent: :destroy
 
   validates :title, presence: true
 
