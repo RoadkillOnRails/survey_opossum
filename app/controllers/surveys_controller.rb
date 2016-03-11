b963199f2b9aa5c305818332fbf5c2e87ec7ccae
Steven Cooper <stevengcooper76@gmail.com>
2016-03-11 14:47:58 -0500

    Survey shows some of the information.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,7 +4,7 @@ class SurveysController < ApplicationController
 
   # GET /surveys/1
   def show
-    @survey = Survey.new(author_id: session[:user_id])
+    @survey = Survey.find_by(author_id: session[:user_id])
   end
 
   # GET /surveys/new
@@ -69,7 +69,7 @@ class SurveysController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_survey
-      @survey = Survey.find(params[:user_id])
+      @survey = Survey.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
