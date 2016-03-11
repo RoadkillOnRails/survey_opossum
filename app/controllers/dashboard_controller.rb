0ae97a8456834684b206e25ae06ad3ebe6aa6f2a
Brian Yarsawich <byarsawich@Brians-MBP.attlocal.net>
2016-03-10 22:19:11 -0500

    data added to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate
 
   def index
-    @surveys = Survey.where(author_id: session[:user_id])
+    @unpublished_surveys = Survey.where('author_id = ? AND token IS ?', session[:user_id], nil)
+    @published_surveys = Survey.where('author_id = ? AND token NOT ?', session[:user_id], nil)
+
   end
 end
