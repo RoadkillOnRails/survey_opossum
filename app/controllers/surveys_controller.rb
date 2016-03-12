30684e90f9abe100aa82c811ccba881588f0b062
Steven Cooper <stevengcooper76@gmail.com>
2016-03-12 15:42:50 -0500

    Jackalope.png in assets folder.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,7 +6,7 @@ class SurveysController < ApplicationController
   def show
     @survey = Survey.find_by(token: params[:token])
     @questions = @survey.questions
-    @answers = @questions.answers
+    # @answers = @questions.answers
   end
 
   # GET /surveys/new
