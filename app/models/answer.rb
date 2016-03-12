3f6f695b33a04f909a56700be19ea8861ecf94d6
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-12 16:11:36 -0500

    resolved merge confilcts

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,11 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
-<<<<<<< HEAD
-
+  belongs_to :taker
+  validates_presence_of :question
   validates :answer, presence: true, if: :required?
 
   def required?
     question.required
   end
-=======
-  belongs_to :taker
-  validates_presence_of :question
->>>>>>> 30684e90f9abe100aa82c811ccba881588f0b062
+
 end
