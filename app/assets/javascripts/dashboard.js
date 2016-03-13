0204e08fb5a04d5d19684b40db6685275fae8169
Jeremy Smith <smithjeremym@gmail.com>
2016-03-13 18:43:56 -0400

    starts share link copying functionality

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,4 +1,5 @@
 // this function syntax allows it to run upon page load instead of before the page loads
+//this function shows and hides the 'no surveys' message in both dashboard sections
 $(function() {
 
 if ($(".pubSurveyList .survey ").length === 0){
@@ -12,5 +13,24 @@ if ($(".savedSurveyList .survey ").length === 0){
 } else {
   //do nothing
 }
+
+});
+
+$(function() {
+
+  $(".surveyShare").click(function() {
+    var textToCopy = $(this).children().filter(".surveyLink").text();
+    console.log(textToCopy);
+
+  // copyTextarea.select();
+
+  // try {
+  //   var successful = document.execCommand('copy');
+  //   var msg = successful ? 'successful' : 'unsuccessful';
+  //   console.log('Copying text command was ' + msg);
+  // } catch (err) {
+  //   console.log('Oops, unable to copy');
+  // }
+  });
 
 });
